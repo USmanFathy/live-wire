f6f7470b8ba33f26d6180a487329249cb8263dd6
usman ahmed fathy <91374170+USmanFathy@users.noreply.github.com>
2023-10-11 15:21:05 +0300

    livewire

---
+++ app/Livewire/PostComponent.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Livewire\Forms\PostForm;
+use App\Models\Post;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+
+class PostComponent extends Component
+{
+    public $isOpen = 0;
+
+    public $postId;
+
+    public PostForm $form;
+    public function create()
+    {
+        $this->reset('form.title','form.body', 'postId');
+        $this->openModal();
+    }
+    public function store()
+    {
+        $this->validate();
+        $this->form->save();
+        session()->flash('success', 'Post created successfully.');
+        $this->reset('form.title','form.body');
+        $this->closeModal();
+    }
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        $this->postId = $id;
+        $this->form->title = $post->title;
+        $this->form->body = $post->body;
+
+        $this->openModal();
+    }
+    public function update()
+    {
+        if ($this->postId) {
+            $post = Post::findOrFail($this->postId);
+            $post->update([
+                'title' => $this->form->title,
+                'body' => $this->form->body,
+            ]);
+            $this->postId='';
+            session()->flash('success', 'Post updated successfully.');
+            $this->closeModal();
+            $this->reset('form.title','form.body');
+        }
+    }
+    public function delete($id)
+    {
+        Post::find($id)->delete();
+        session()->flash('success', 'Post deleted successfully.');
+    }
+    public function openModal(){
+        $this->isOpen=true;
+        $this->resetValidation();
+    }
+    public function closeModal(){
+        $this->isOpen=false;
+    }
+
+    public function render()
+    {
+        return view('livewire.post-component',['posts' => Post::paginate(5)]);
+    }
+}
